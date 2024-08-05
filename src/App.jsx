465cc79e15fc2c206f0e1a045b19c6d7b9c1c0d4
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-08-05 15:01:42 +0200

    fetchContacts, addContact, deleteContact

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import css from "./App.module.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./components/redux/contactsOps";
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.phonebook}>
+      <div className={css.container}>
+        <h1 className={css.title}>Phonebook</h1>
+        <div className={css.phonebookContentWrap}>
+          <div className={css.containerCol}>
+            <ContactForm />
+          </div>
+          <div className={css.containerCol}>
+            <div className={css.phonebookContactsWrap}>
+              <SearchBox />
+              <ContactList />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
